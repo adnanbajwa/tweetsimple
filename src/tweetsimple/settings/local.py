5d7674f321fe7e0b9c8e39fe315869833f0ad206
adnanbajwa <adnan.bajwa163@gmail.com>
2017-06-20 21:03:20 +0300

    setting up static files

--- src/tweetsimple/settings/local.py
+++ src/tweetsimple/settings/local.py
@@ -118,3 +118,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    # will not be served, long term use
+    os.path.join(BASE_DIR, "static-storage"),
+]
+
+# will be served
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static-serve")
+
+# For production, its uses another server CDN (content delivery network) e.g 
+# STATIC_ROOT = "webapps/abc/static"
+
+
+
+
